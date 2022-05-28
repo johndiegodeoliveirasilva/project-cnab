c9a99a99eb021d14f908dd613786ae1ef1a047b1
John Diego <johnoliveirasilva9@gmail.com>
2022-05-28 20:46:16 -0300

    test importation file

--- spec/factories/import_file.rb
+++ spec/factories/import_file.rb
@@ -3,6 +3,8 @@ FactoryBot.define do
     value { Faker::Number.decimal(l_digits: 3, r_digits: 3) }
     cpf { Faker::Number.leading_zero_number(digits: 11) }
     card { Faker::Number.leading_zero_number(digits: 12)}
+    data { Faker::Time.between(from: DateTime.now - 1, to: DateTime.now) }
+    hour { Faker::Time.between(from: DateTime.now - 1, to: DateTime.now) }
     association :kind_transaction
     association :company
   end
