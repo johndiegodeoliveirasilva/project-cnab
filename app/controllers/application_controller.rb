0309903836b89ee4e95c8bb09bab9eab6069e45e
John Diego <johnoliveirasilva9@gmail.com>
2022-05-30 20:55:06 -0300

    configure devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   include ActionController::MimeResponds
   include Pagy::Backend
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+  protect_from_forgery with: :reset_session
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name phone city state])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 require 'sidekiq/web'
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
   mount Sidekiq::Web => '/sidekiq'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
