6e9683c52e2335d1420b27a9e6c5cd27928ea637
John Diego <johnoliveirasilva9@gmail.com>
2022-05-30 22:33:42 -0300

    add templates devise

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,10 +1,17 @@
 class Users::SessionsController < Devise::SessionsController
   respond_to :json
 
+  def create
+    super
+  end
   private
 
   def respond_with(resource, _opts = {})
-    render json: { message: 'You are logged in.' }, status: :ok
+    
+    respond_to do |format|
+      format.json { render json: { message: 'You are logged in.' }, status: :ok }
+      format.html
+    end
   end
 
   def respond_to_on_destroy
@@ -14,10 +21,16 @@ class Users::SessionsController < Devise::SessionsController
   end
 
   def log_out_success
-    render json: { message: 'You are logged out.' }, status: :ok
+    respond_to do |format|
+      format.json { render json: { message: 'You are logged out.' }, status: :ok }
+      format.html
+    end
   end
 
   def logout_out_failure
-    render json: { message: 'Failure to logout'}, status: :unauthorized
+    respond_to do |format|
+      format.json { render json: {message: 'Failure to logout'}, status: :unauthorized }
+      format.html
+    end
   end
 end
